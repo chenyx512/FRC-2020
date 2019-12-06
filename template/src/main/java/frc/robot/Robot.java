9c29304e1441362db7d0aa48a9337d99e5811411
chenyx <yuxuanchen@stmarksschool.org>
2019-12-06 14:48:29 -0500

    Add initial template

---
+++ template/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+    @Override
+    public void robotInit() {
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+}
