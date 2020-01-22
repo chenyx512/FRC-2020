7988f5658c8b0995b899017e04d81567992a1911
andriabao <58490077+andriabao@users.noreply.github.com>
2020-01-22 15:12:00 -0500

    reflectiveTape

--- reflectiveTape/main.py
+++ reflectiveTape/main.py
@@ -5,8 +5,9 @@ def show(img):
     cv2.imshow('img', img)
     cv2.waitKey(1)
 
-vc = cv2.VideoCapture(1)
-vc.set(cv2.CAP_PROP_EXPOSURE,-9)
+vc = cv2.VideoCapture(0)
+vc.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+vc.set(cv2.CAP_PROP_EXPOSURE,-8)
 vc.set(cv2.CAP_PROP_FRAME_HEIGHT,1280);
 vc.set(cv2.CAP_PROP_FRAME_WIDTH,720);
 
@@ -19,11 +20,16 @@ while True:
     thresh = cv2.inRange(frame_HSV, Constants.HSV_LOW, Constants.HSV_HIGH)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contour = max(contours, key=cv2.contourArea)
-    cv2.drawContours(frame, [contour], -1, (0, 0, 255))
 
-    # M = cv2.moments(contour)
-    # center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-    #
-    # pts.appendleft(center)
+    if len(contours) > 0:
+        contour = max(contours, key=cv2.contourArea)
+        rect = cv2.boundingRect(contour)
+        pass
+    else:
+        print("NO TARGET")
+        continue
+
+    cv2.drawContours(frame, [contour], -1, (0, 0, 255))
+    cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0))
+
     show(frame)
